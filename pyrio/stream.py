9f80f661c825e2c92a552c7340961727688a79ad
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-09-04 15:37:32 +0300

    refactored use controller functions

--- pyrio/stream.py
+++ pyrio/stream.py
@@ -231,32 +231,54 @@ class Stream:
         import inspect
 
         if args:
-            raise ValueError("Use keyword arguments only")  # TODO: ?!
+            raise ValueError("Use keyword arguments only")
 
-        # handle functions with no signature
-        if it_function.__name__ in ("chain", "islice", "product"):
-            if it_function.__name__ == "product":
-                self._iterable = it_function(*self._iterable, *kwargs.values())
-            else:
-                self._iterable = it_function(self._iterable, *kwargs.values())
+        if self._handle_no_signature_functions(it_function, **kwargs):
             return self
 
         signature = inspect.signature(it_function).parameters
+        if self._handle_no_kwargs_functions(signature, it_function, **kwargs):
+            return self
 
-        # handle functions that take iterable as args
+        return self._handle_default_signature_functions(it_function, kwargs, signature)
+
+    def _handle_no_signature_functions(self, it_function, **kwargs):
+        NO_SIGNATURE_FUNCTIONS = ["chain", "islice", "product", "repeat"]
+
+        if it_function.__name__ not in NO_SIGNATURE_FUNCTIONS:
+            return False
+
+        if it_function.__name__ == "product":
+            if isinstance(self._iterable, range):
+                self._iterable = it_function(self._iterable, **kwargs)
+            else:
+                self._iterable = it_function(*self._iterable, **kwargs)
+            return True
+
+        # functions like 'chain' don't expect key-word arguments
+        self._iterable = it_function(self._iterable, *kwargs.values())
+        return True
+
+    def _handle_no_kwargs_functions(self, signature, it_function, **kwargs):
+        NO_KWARGS_FUNCTIONS = ["dropwhile", "filterfalse", "starmap"]
+
+        # handle functions that take only iterable as arg
         if len(signature.keys()) == 1 and "iterable" in signature:
             self._iterable = it_function(self._iterable)
-            return self
+            return True
 
-        # if signature.keys() == {"predicate", "iterable"}:
-        if it_function.__name__ in ("dropwhile", "filterfalse"):
-            self._iterable = it_function(kwargs.get("predicate", None), self._iterable)
-            return self
+        # handle functions that take no kwargs
+        if it_function.__name__ in NO_KWARGS_FUNCTIONS:
+            # self._iterable = it_function(kwargs.get("function", None), self._iterable)
+            # TODO: starmap uses function instead of predicate
+            self._iterable = it_function(*kwargs.values(), self._iterable)
+            return True
+        return False
 
+    def _handle_default_signature_functions(self, it_function, kwargs, signature):
         if "iterable" in signature:
             kwargs["iterable"] = self._iterable
         elif "data" in signature:
             kwargs["data"] = self._iterable
-
         self._iterable = it_function(**kwargs)
         return self

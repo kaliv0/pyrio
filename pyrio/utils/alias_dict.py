676467b78b842b27d10af09e68b44982a5601a24
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-12-12 21:27:29 +0200

    extended AliasDict internal APIs

--- pyrio/utils/alias_dict.py
+++ pyrio/utils/alias_dict.py
@@ -1,9 +1,6 @@
 from collections import UserDict
-from itertools import chain
 
 from pyrio.exceptions import AliasError
-
-# TODO: use self.data instead of self.keys()/.items() etc??
 
 
 class AliasDict(UserDict):
@@ -15,34 +12,48 @@ class AliasDict(UserDict):
 
     def add_alias(self, key, alias):
         if alias == key:
-            raise AliasError("Key and corresponding alias cannot be equal")
-        if key not in self.keys():
-            raise KeyError(f"Key '{key}' not found")
+            raise AliasError(f"Key and corresponding alias cannot be equal: '{key}'")
+        if key not in self.data.keys():
+            # raise KeyError(f"Key '{key}' not found")
+            raise KeyError(key)
         self._VAL_DICT[alias] = key
 
+    # TODO: add bulk delete -> remove_aliases(*alias_list)
     def remove_alias(self, alias):
-        # TODO: raise if not found?
-        self._VAL_DICT.pop(alias, None)
+        self._VAL_DICT.__delitem__(alias)
 
-    # TODO: handle if key is removed -> cascade_delete all aliases -> add test
+        # if self._VAL_DICT.pop(alias, None) is None:
+        #   # TODO: use custom error -> AliasNotFound?
+        # raise KeyError(f"Alias '{alias}' not found")
 
-    def iterkeys(self):
-        return tuple(chain(self.keys(), self._VAL_DICT.keys()))
+    def aliases(self):
+        return self._VAL_DICT.keys()
+
+    # TODO: show keys only -> add method?
+
+    def keys(self):
+        # return tuple(chain(self.data.keys(), self._VAL_DICT.keys()))
+
+        combined_dict = {**self.data, **self._VAL_DICT}
+        return combined_dict.keys()
 
     # TODO: do we need this?
-    def itervalues(self):
-        return tuple(self.values())
+    def values(self):
+        return self.data.values()
 
-    def iteritems(self):
-        return tuple(
-            (k, v) for k, v in chain(self.items(), ((k, self[v]) for k, v in self._VAL_DICT.items()))
-        )
+    def items(self):
+        # return tuple(
+        #     (k, v) for k, v in chain(self.data.items(), ((k, self[v]) for k, v in self._VAL_DICT.items()))
+        # )
+        combined_dict = {**self.data, **self._VAL_DICT}
+        return combined_dict.items()
 
     def __missing__(self, key):
         try:
             return super().__getitem__(self._VAL_DICT[key])
         except AttributeError:
-            raise KeyError(f"Key '{key}' not found")
+            # raise KeyError(f"Key '{key}' not found")
+            raise KeyError(key)
 
     def __setitem__(self, key, value):
         try:
@@ -50,3 +61,28 @@ class AliasDict(UserDict):
         except KeyError:
             pass
         super().__setitem__(key, value)
+
+        # TODO: __getitem__?
+
+    def __delitem__(self, key):
+        try:
+            self.data.__delitem__(key)
+        except KeyError:
+            # TODO: in cases we try to delete alias e.g. via pop()
+            pass
+
+        # for k, v in tuple(self._VAL_DICT.items()):
+        #     if v == key:
+        #         self._VAL_DICT.pop(k)
+        self._VAL_DICT = {k: v for k, v in self._VAL_DICT.items() if v != key}
+        # TODO: optimize
+
+    def __contains__(self, item):
+        return item in set(self.keys())
+
+    def __iter__(self):
+        for item in self.keys():
+            yield item
+
+    def __repr__(self):
+        return f"AliasDict({self.items()})"
--- tests/test_alias_dict.py
+++ tests/test_alias_dict.py
@@ -1,5 +1,7 @@
 import pytest
 
+from pyrio.exceptions import AliasError
+
 
 def test_alias_dict(alias_dict):
     assert alias_dict[".toml"] == {"callable": "load", "import_mod": "tomli", "read_mode": "r"}
@@ -19,42 +21,102 @@ def test_add_alias(alias_dict):
     )
 
 
+def test_add_alias_raises(alias_dict):
+    with pytest.raises(AliasError, match="Key and corresponding alias cannot be equal: '.toml'"):
+        alias_dict.add_alias(".toml", ".toml")
+    # assert str(e.value) == "Key and corresponding alias cannot be equal: '.toml'"
+
+
 def test_update_alias(alias_dict):
     alias_dict.add_alias(".toml", ".yml")
-    assert alias_dict.iteritems() == (
+    assert list(alias_dict.items()) == [
         (".json", {"callable": "load", "import_mod": "json", "read_mode": "r"}),
         (".yaml", {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}),
         (".toml", {"callable": "load", "import_mod": "tomli", "read_mode": "r"}),
-        (".yml", {"callable": "load", "import_mod": "tomli", "read_mode": "r"}),
-    )
+        (".yml", ".toml"),
+    ]
 
 
 def test_update_alias_raises(alias_dict):
-    with pytest.raises(KeyError, match="Key '.foo' not found"):
+    with pytest.raises(KeyError, match=".foo"):
         alias_dict.add_alias(".foo", ".bar")
+    # assert e.value.args[0] == "Key '.foo' not found"
 
 
 def test_remove_alias(alias_dict):
-    assert alias_dict.iterkeys() == (".json", ".yaml", ".toml", ".yml")
+    assert list(alias_dict.keys()) == [".json", ".yaml", ".toml", ".yml"]
     alias_dict.remove_alias(".yml")
-    assert alias_dict.iterkeys() == (".json", ".yaml", ".toml")
-    assert alias_dict.iteritems() == (
+    assert list(alias_dict.keys()) == [".json", ".yaml", ".toml"]
+    assert list(alias_dict.items()) == [
         (".json", {"callable": "load", "import_mod": "json", "read_mode": "r"}),
         (".yaml", {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}),
         (".toml", {"callable": "load", "import_mod": "tomli", "read_mode": "r"}),
-    )
+    ]
+
+
+def test_remove_alias_raises(alias_dict):
+    assert list(alias_dict.keys()) == [".json", ".yaml", ".toml", ".yml"]
+    with pytest.raises(KeyError, match=".foo"):
+        alias_dict.remove_alias(".foo")
+    # assert str(e.value) == "Alias '.foo' not found"
+
+
+def test_aliases(alias_dict):
+    alias_dict.add_alias(".toml", ".tml")
+    alias_dict.add_alias(".json", ".jsn")
+    alias_dict.add_alias(".json", ".whaaaaat!")
+    assert list(alias_dict.aliases()) == [".yml", ".tml", ".jsn", ".whaaaaat!"]
 
 
 def test_dictviews(alias_dict):
-    assert alias_dict.iterkeys() == (".json", ".yaml", ".toml", ".yml")
-    assert alias_dict.itervalues() == (
+    assert list(alias_dict.keys()) == [".json", ".yaml", ".toml", ".yml"]
+    assert list(alias_dict.values()) == [
         {"import_mod": "json", "callable": "load", "read_mode": "r"},
         {"import_mod": "yaml", "callable": "safe_load", "read_mode": "r"},
         {"import_mod": "tomli", "callable": "load", "read_mode": "r"},
-    )
-    assert alias_dict.iteritems() == (
+    ]
+    assert list(alias_dict.items()) == [
         (".json", {"callable": "load", "import_mod": "json", "read_mode": "r"}),
         (".yaml", {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}),
         (".toml", {"callable": "load", "import_mod": "tomli", "read_mode": "r"}),
-        (".yml", {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}),
+        (".yml", ".yaml"),
+    ]
+
+
+def test_remove_key_and_aliases(alias_dict):
+    assert list(alias_dict.keys()) == [".json", ".yaml", ".toml", ".yml"]
+    alias_dict.pop(".yaml")
+    assert list(alias_dict.keys()) == [".json", ".toml"]
+
+
+def test_contains(alias_dict):
+    alias_dict.add_alias(".toml", ".tml")
+    assert (".toml" in alias_dict) is True
+    assert (".tml" in alias_dict) is True
+    assert (".foo" in alias_dict) is False
+
+
+def test_get(alias_dict):
+    assert alias_dict.get(".yaml") == {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}
+    assert alias_dict.get(".yml") == {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}
+    assert alias_dict.get(".foo") is None
+
+
+def test_pop(alias_dict):
+    assert alias_dict.pop(".yml") == {"callable": "safe_load", "import_mod": "yaml", "read_mode": "r"}
+    assert ".yaml" in alias_dict  # removing the alias doesn't remove the key
+
+
+def test_iter(alias_dict):
+    assert [k for k in alias_dict] == [".json", ".yaml", ".toml", ".yml"]
+
+
+def test_repr(alias_dict):
+    assert str(alias_dict) == (
+        "AliasDict(dict_items(["
+        "('.json', {'import_mod': 'json', 'callable': 'load', 'read_mode': 'r'}), "
+        "('.yaml', {'import_mod': 'yaml', 'callable': 'safe_load', 'read_mode': 'r'}), "
+        "('.toml', {'import_mod': 'tomli', 'callable': 'load', 'read_mode': 'r'}), "
+        "('.yml', '.yaml')"
+        "]))"
     )
